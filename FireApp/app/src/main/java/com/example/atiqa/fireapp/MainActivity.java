935f109a284d918f6bdade7f17f03190f3547713
atiqahammed <atiqahammedshamim@gmail.com>
2018-02-02 09:33:21 +0600

    Firebase class not working

--- FireApp/app/src/main/java/com/example/atiqa/fireapp/MainActivity.java
+++ FireApp/app/src/main/java/com/example/atiqa/fireapp/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.atiqa.fireapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Firebase.setAndroidContext(this);
+
     }
 }
