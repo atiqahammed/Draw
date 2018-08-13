7a3383ba863ed3948c80226192cd4c8128cb7ab2
atiqahammed <atiqahammedshamim@gmail.com>
2018-08-13 10:25:50 +0600

    onTouch method is working

--- app/src/main/java/com/example/atiq/android_paint/MainActivity.java
+++ app/src/main/java/com/example/atiq/android_paint/MainActivity.java
@@ -5,8 +5,10 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -16,6 +18,13 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Desenho";
+    private ShapeDrawable rectangle;
+    private Paint paint;
+    private float currX, currY;
+    private Rect blue, gray;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
