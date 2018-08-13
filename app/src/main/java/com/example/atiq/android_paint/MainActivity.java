5736e90d4560cd65e8c2545c0f9855220acc2199
atiqahammed <atiqahammedshamim@gmail.com>
2018-08-13 09:58:12 +0600

    touch listern has been added

--- app/src/main/java/com/example/atiq/android_paint/MainActivity.java
+++ app/src/main/java/com/example/atiq/android_paint/MainActivity.java
@@ -4,11 +4,16 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.LinearLayout;
+import android.graphics.PointF;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         int top1 = 10;
         int right1 = 30;
         int bottom1 = 30;
+
+        //
 
         for(int j=0; j< 31; j++)
         {
@@ -54,4 +61,24 @@ public class MainActivity extends AppCompatActivity {
         l1.setBackgroundDrawable(new BitmapDrawable(bg));
 
     }
+
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    private float mPreviousX;
+    private float mPreviousY;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
+
+        Toast.makeText(this, "there was a touch", Toast.LENGTH_SHORT).show();
+
+
+       // mPreviousX = x;
+        //mPreviousY = y;
+        return true;
+    }
+
+
 }
