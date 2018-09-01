03422059382938b26e3fb13ed3f21be542ae37b5
atiqahammed <atiqahammedshamim@gmail.com>
2018-09-01 08:55:01 +0600

    nothing produvtive has been done

--- app/src/main/java/com/example/atiq/android_paint/MainActivity.java
+++ app/src/main/java/com/example/atiq/android_paint/MainActivity.java
@@ -63,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
         //Log.e("height", "" + height);
 
 
-
         myBitMap = Bitmap.createBitmap(width,height, Bitmap.Config.ARGB_8888);
         canvas = new Canvas(myBitMap);
     }
@@ -83,22 +82,98 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
 
 
-
         String state = Environment.getExternalStorageState();
         if (Environment.MEDIA_MOUNTED.equals(state)) {
-            Toast.makeText(this, "Drawing has been saved", Toast.LENGTH_SHORT).show();
+            //  Toast.makeText(this, "Drawing has been saved", Toast.LENGTH_SHORT).show();
 
             //return true;
         }
 
+        try {
+
+            File dir = new File(Environment.getExternalStorageDirectory(), "Test");
+            if (!dir.exists() || !dir.isDirectory())
+                dir.mkdirs();
+
+            String path = dir.getAbsolutePath();
+            //Toast.makeText(this, path, Toast.LENGTH_SHORT).show();
+            //Log.d(TAG, path);                     //log show the path
+            // File file = new File(dir.getAbsolutePath() + "/Pic.jpg");
+            //Log.d(TAG, file.getAbsolutePath());   //again path is shown here
+
+            File file = new File(new File(path), "abc.jpg");
+
+            Canvas canvas = new Canvas(myBitMap);
+
+                FileOutputStream mFileOutStream = new FileOutputStream(file);
+
+                //v.draw(canvas);
+                myBitMap.compress(Bitmap.CompressFormat.PNG, 90, mFileOutStream);
+
+                mFileOutStream.flush();
+                mFileOutStream.close();
+                String url = MediaStore.Images.Media.insertImage(getContentResolver(),
+                        myBitMap, "title", null);
+
+                Toast.makeText(this, "URI: " + url, Toast.LENGTH_SHORT).show();
+
+                OutputStream outStream = new FileOutputStream(file);
+                //String bytes = "khdfkas";
+                //outStream.write(bytes.getBytes());
+                //outStream.close();
+                //Log.d(TAG, "onPictureTaken - wrote bytes: " + bytes.length);   //fail here
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "not done");                       //error is here (this exception is thrown)
+            } catch (IOException e) {
+                Log.d(TAG, "not");
+            } finally {
+            }
 
 
-        Toast.makeText(this, "Drawing has been saved", Toast.LENGTH_SHORT).show();
+
+
+
+    /*
+    int n = 1;
+
+        Intent camera = new Intent(
+                android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //Folder is already created
+        String dirName = Environment.getExternalStorageDirectory().getPath()
+                + "/Test" + n + ".png";
+
+        Uri uriSavedImage = Uri.fromFile(new File(dirName));
+        camera.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+        startActivityForResult(camera, 1);
+
+        n++;
+
+
+       // File filew = getPublicAlbumStorageDir("test");
+
+        //Toast.makeText(this, "Drawing has been saved", Toast.LENGTH_SHORT).show();
+        /*
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
-    }
+        */
+
+        }
+
+    /*
+
+    public File getPublicAlbumStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+           // Log.e(LOG_TAG, "Directory not created");
+            Toast.makeText(this, "not created", Toast.LENGTH_SHORT).show();
+        } else Toast.makeText(this, "created", Toast.LENGTH_SHORT).show();
+        return file;
+    }*/
 
 
 
